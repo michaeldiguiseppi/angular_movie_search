99e18854167701d5bc078498200a2b2a2a8c2ee0
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-06 15:55:20 -0600

    routes.

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,10 @@ var app = angular.module('myApp', ["ngRoute"]);
 app.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/movies', {
-      templateUrl: 'partials/searchAll.html',
+      templateUrl: 'angular_search_app/partials/searchAll.html',
     })
     .when('/oneMovie', {
-      templateUrl: 'partials/searchOne.html',
+      templateUrl: 'angular_search_app/partials/searchOne.html',
     });
     $locationProvider.html5Mode({
       enabled: true,
