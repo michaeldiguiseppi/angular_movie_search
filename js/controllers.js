8def16f3dd221d0fd06f3de8edbaad69929ced5c
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-06 15:13:18 -0600

    Added single search, type search, and rotten tomatoes search.

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,45 @@
 app.controller('searchController', ['$scope', '$http', function($scope, $http) {
   $scope.searchTerm = '';
+  $scope.tomatoes = false;
   $scope.searchMovie = function(body) {
     var term = body;
+    var url = 'http://www.omdbapi.com/?s='+term;
+    if ($scope.type) {
+      url += '&type='+$scope.type;
+    }
     $http({
       method: 'GET',
-      url: 'http://www.omdbapi.com/?s='+term,
+      url: url,
     }).then(function(response) {
-      console.log(response.data.Search);
       $scope.movies = response.data.Search;
     });
   };
   $scope.searchOne = function(body) {
-    console.log(body);
     var id = body;
+    var extras = '&tomatoes=true';
+    var url = 'http://www.omdbapi.com/?i=';
+    if ($scope.tomatoes) {
+      url += (id + extras);
+    } else {
+      url += id;
+    }
     $http({
       method: 'GET',
-      url: 'http://www.omdbapi.com/?i='+id,
+      url: url,
     }).then(function(response) {
-      console.log(response);
-      $scope.fields = response.data;
+      $scope.fields = [];
+      $scope.title = response.data.Title;
+      $scope.synopsis = response.data.Plot;
+      $scope.starring = response.data.Actors;
+      var fields = Object.keys(response.data);
+      fields.forEach(function(field) {
+        if (field !== 'Plot' && field !== 'Title' && field !== 'Poster' && field !== 'Actors') {
+          $scope.fields.push({
+            field: field,
+            value: response.data[field],
+          });
+        }
+      });
       $scope.img = response.data.Poster;
     });
   };
